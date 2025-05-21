7808b22af5cb667a0fe91f3ced74bbd6f44b358d
Justin <justintbalbuena@gmail.com>
2025-05-20 23:59:17 -0400

    updated with impossible dash

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const container1 = document.querySelector(".favorite_project1")
     const container2 = document.querySelector(".favorite_project2")
     const container3 = document.querySelector(".favorite_project3")
+    const container4 = document.querySelector(".favorite_project4")
 
     const observer = new IntersectionObserver(
         (entries) => {
@@ -12,6 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     container2.classList.add("sliding_from_right")
 
                     container3.classList.add("sliding_from_left")
+
+                    container4.classList.add("sliding_from_right")
 
                     observer.unobserve(container1)
                 }
